bc2f1cf8df0759d2ba4e029723e633a704a82cf5
dody <dodyrw@gmail.com>
2011-11-27 21:05:20 +0700

    hide claim button if owned by someone

--- hepi/hepi_detail.php
+++ hepi/hepi_detail.php
@@ -2,7 +2,7 @@
   $t_hepi['id'] = 'hepi';
   $t_hepi['name'] = 'hepi';
   $t_hepi['author'] = 'Dody Rachmat Wicaksono';
-  $t_hepi['web'] = 'http://www.indexupremium.com';
+  $t_hepi['web'] = 'http://www.nicecoder.com';
   $t_hepi['preview'] = '';
   $t_hepi['position'] = array(
     'topmenu1',
@@ -18,7 +18,6 @@
     'footer1',
     'footer2',
     'footer3',
-    'footer4',
-    'footer'
+    'footer4'
   );
 ?>
